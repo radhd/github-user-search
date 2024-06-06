21535fb278de6d01be5db14ff171d4f84fc7533d
Nika M. <merabishvilinikusha@gmail.com>
2024-06-06 22:58:52 +0400

    Set input value

--- src/components/SearchDev.tsx
+++ src/components/SearchDev.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 function SearchDev() {
-  
+  const [search, setSearch] = useState<string>("");
+
+  const onClick = () => {};
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div className="">
       <input
+        onChange={(event) => onChange(event)}
+        value={search}
         type="text"
         className="border border-slate-500 focus:outline-none"
       />
-      <button className="bg-blue-500">Search</button>
+      <button onClick={onClick} className="bg-blue-500">
+        Search
+      </button>
     </div>
   );
 }
