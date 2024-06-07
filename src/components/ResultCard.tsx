3b827430e93a7ed2873a1200a0d0fbbc0d86b5d6
Nika M. <merabishvilinikusha@gmail.com>
2024-06-07 16:00:08 +0400

    Created fetched data logic on search

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,4 +1,13 @@
 import { Users } from "../userInterfaces";
+import {
+  BeakerIcon,
+  EnvelopeIcon,
+  GlobeEuropeAfricaIcon,
+  LinkIcon,
+  MapIcon,
+  MapPinIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/solid";
 
 interface ResultCardProps {
   userArray: Users[];
@@ -12,29 +21,43 @@ function ResultCard({ userArray }: ResultCardProps): React.ReactElement {
           <div>
             <img src={user.avatar_url} alt="" />
           </div>
-          <span>{user.name}</span>
-          <span>{user.created_at}</span>
-          <span>{user.login}</span>
-          <span>{user.bio ? user.bio : "This profile has no bio"}</span>
+          <div>{user.name}</div>
+          <div>{user.created_at}</div>
+          <div>{user.login}</div>
+          <div>{user.bio ? user.bio : "This profile has no bio"}</div>
           {/* REPOS INFO/FOLLOWERS/FOLLOWING */}
           <div>
             <div>
-              <span>Repos</span>
-              <span>{user.public_repos}</span>
+              <div>Repos</div>
+              <div>{user.public_repos}</div>
             </div>
             <div>
-              <span>Followers</span>
-              <span>{user.followers}</span>
+              <div>Followers</div>
+              <div>{user.followers}</div>
             </div>
             <div>
-              <span>Following</span>
-              <span>{user.following}</span>
+              <div>Following</div>
+              <div>{user.following}</div>
             </div>
           </div>
           {/* Location/GITHUBURL */}
+
           <div>
             <div>
-              
+              <MapPinIcon className="size-5 text-blue-500" />
+              <div>{user.location ? user.location : "Not Available"}</div>
+            </div>
+            <div>
+              <LinkIcon className="size-5" />
+              <div>{user.html_url}</div>
+            </div>
+            <div>
+              <EnvelopeIcon className="size-5" />
+              <div>{user.email ? user.email : "Not Available"}</div>
+            </div>
+            <div>
+              <UserGroupIcon className="size-5" />
+              <div>{user.type}</div>
             </div>
           </div>
         </div>
